eee1312bb298e3d44c6c25d3689031445dc506da
William Clifford <William.Clifford@TUDublin.ie>
2026-01-29 11:00:10 +0000

    made a change to our testing file

--- PerformanceRating_test.py
+++ PerformanceRating_test.py
@@ -1,7 +1,7 @@
 import unittest
 from PerformanceRating import PerformanceRating
 
-class TestPerformanceRating(unittest.TestCase):
+class TestPerformanceRating_test(unittest.TestCase):
 
     # Equivalence Classes:
     # EC1: time < 0
